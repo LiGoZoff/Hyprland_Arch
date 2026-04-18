cd25438fb7799303f51c5fdd1f7d86e98115d79b
LiGoZoff <0718snak0718@gmail.com>
2026-04-18 23:05:06 +0300

    Update secureboot.sh

--- scripts/secureboot.sh
+++ scripts/secureboot.sh
@@ -2,12 +2,11 @@
 sudo pacman -S sbsigntools mokutil refind efitools sbctl --noconfirm 
 yay -S shim-signed --noconfirm 
 sudo refind-install
-sbctl setup --migrate
-sbctl create-keys
-sbctl enroll-keys -m
-sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
-sbctl sign -s /boot/EFI/Linux/*.efi
-sbctl sign -s /efi/EFI/Linux/*.efi
-sbctl sign -s /boot/EFI/refind/refind_x64.efi
-sbctl sign -s /efi/EFI/refind/refind_x64.efi
-bootctl install
+sudo sbctl create-keys
+sudo sbctl enroll-keys -m
+sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+sudo sbctl sign -s /boot/EFI/Linux/*.efi
+sudo sbctl sign -s /efi/EFI/Linux/*.efi
+sudo sbctl sign -s /boot/EFI/refind/refind_x64.efi
+sudo sbctl sign -s /efi/EFI/refind/refind_x64.efi
+sudo bootctl install
