3fde25814b7e109784fa03f324cae99b02defc43
LiGoZoff <0718snak0718@gmail.com>
2026-05-15 01:14:00 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -442,3 +442,5 @@ clear
 echo "Successfuly installation"
 chsh -s /bin/fish
 sudo -s /usr/bin/fish
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install IlanCosman/tide@v6
