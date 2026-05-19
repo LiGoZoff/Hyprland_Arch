c0ee492008da148a879087a0163db02c19192a4a
LiGoZoff <0718snak0718@gmail.com>
2026-05-19 08:59:02 +0300

    Add files via upload

--- config/waypaper/post_script.sh
+++ config/waypaper/post_script.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
+CURRENT_BACKEND=$(grep "^backend\s*=" "$HOME/.config/waypaper/config.ini" | sed 's/.*=\s*//' | xargs)
+LAST_BACKEND_FILE="$HOME/.config/waypaper/last_backend"
+ALL_BACKENDS=("awww" "linux-wallpaperengine" "mpvpaper" "swaybg" "hyprpaper")
+
+if [ -f "$LAST_BACKEND_FILE" ]; then
+    LAST_BACKEND=$(cat "$LAST_BACKEND_FILE")
+fi
+
+if
+    [ "$CURRENT_BACKEND" == "awww" ]; then
+        pkill -9 -f "linux-wallpaperengine|mpvpaper|swaybg|hyprpaper"
+    elif [ "$CURRENT_BACKEND" == "linux-wallpaperengine" ]; then
+        pkill -9 -f "awww-daemon|mpvpaper|swaybg|hyprpaper"
+    elif [ "$CURRENT_BACKEND" == "mpvpaper" ]; then
+        pkill -9 -f "awww-daemon|linux-wallpaperengine|swaybg|hyprpaper"
+    elif [ "$CURRENT_BACKEND" == "swaybg" ]; then
+        pkill -9 -f "awww-daemon|linux-wallpaperengine|mpvpaper|hyprpaper"
+    elif [ "$CURRENT_BACKEND" == "hyprpaper" ]; then
+        pkill -9 -f "awww-daemon|linux-wallpaperengine|mpvpaper|swaybg"
+    fi
+
+
+
+echo "$CURRENT_BACKEND" > "$LAST_BACKEND_FILE"
+
 RAW_PATH="$1"
 WALLPAPER_DIR=$(dirname "$RAW_PATH")
 TARGET="$HOME/Pictures/Wallpapers/.wallpaper_current.jpg"
 
-# 1. Поиск видеофайла (mp4, mkv, webm)
 VIDEO_FILE=$(find "$WALLPAPER_DIR" -type f \( -name "*.mp4" -o -name "*.mkv" -o -name "*.webm" \) -printf "%s %p\n" | sort -rn | head -n1 | cut -d' ' -f2-)
 
-# 2. Логика извлечения
 if [ -n "$VIDEO_FILE" ]; then
-    # Если видео найдено, берем первый кадр из него
     ffmpeg -y -i "$VIDEO_FILE" -frames:v 1 -f image2 "$TARGET" -loglevel error
 else
-    # Если видео нет (это сцена), берем ПЕРВЫЙ кадр из preview.gif
-    # Это гарантирует, что мы получим картинку, а не черный фон от текстур
     magick "${RAW_PATH}[0]" "$TARGET"
 fi
 
-# 3. Символическая ссылка для совместимости
-ln -sf "$TARGET" "$HOME/Pictures/Wallpapers/.wallpaper_current"
-
-# 4. Копирование для SDDM
 cp "$TARGET" "/usr/share/sddm/themes/simple-sddm/Backgrounds/wallpaper_current.gif"
 
-pkill -SIGUSR1 waybar
-# 5. Работа с Pywal
 rm -rf "$HOME/.cache/wal"
 wal -i "$TARGET"
 bash ~/.config/waybar/scripts/generate-config.sh
 
 pkill -SIGUSR2 waybar
 hyprctl reload
-pkill -SIGUSR1 waybar
 
 bash ~/.config/hypr/Themes/pywal-obsidian/pywal-obsidian.sh "$HOME/Документы/Obsidian"
 pywalfox install
 walcord
+swaync-client --reload-css
+systemctl --user restart swaync
