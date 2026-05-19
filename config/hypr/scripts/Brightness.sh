6b9e209e027ece3c723086ef330a5c0611f306c8
LiGoZoff <0718snak0718@gmail.com>
2026-05-19 08:51:05 +0300

    Add files via upload

--- config/hypr/scripts/Brightness.sh
+++ config/hypr/scripts/Brightness.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 
-# Get brightness
 get_backlight() {
 	echo $(brightnessctl -m | cut -d, -f4)
 }
 
-# Get icons
 get_icon() {
 	current=$(get_backlight | sed 's/%//')
 }
 
-# Notify
 notify_user() {
-	notify-send -h string:x-canonical-private-synchronous:sys-notify -u low "Brightness : $current%"
+	notify-send -h string:x-canonical-private-synchronous:sys-notify -h string:transient:true -u low "Brightness : $current%"
 }
 
-# Change brightness
 change_backlight() {
 	brightnessctl set "$1" && get_icon && notify_user
 }
 
-# Execute accordingly
 case "$1" in
 	"--get")
 		get_backlight
